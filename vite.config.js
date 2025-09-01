1f962c9a43626d0ba1f6632698bb6834aac8ce53
lostfiddler <h3lloiain@gmail.com>
2025-08-31 21:41:13 -0500

    modify vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
+    build: {
+        target: 'es2022'
+    },
     plugins: [
         wasm(),
     ]
