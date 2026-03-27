c18d9696535dffc01ae3de446498eb6ef73bea82
Ian Parkinson <h3lloiain@gmail.com>
2026-03-27 03:43:03 -0500

    update landing page, still needs work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,42 @@ export default () => {
                 How did you find this? What are you doing here? Well since
                 you're here I guess I love you.
             </p>
+            <p>Some cool libraries to check out:</p>
+            <ul>
+                <li>
+                    <a href="https://roughjs.com/">rough.js</a>
+                </li>
+                <li>
+                    <a href="https://ptsjs.org/">pts.js</a>
+                </li>
+                <li>
+                    <a href="https://zzz.dog/">Zdog</a>
+                </li>
+                <li>
+                    <a href="https://www.kesiev.com/akihabara/">akihabara</a>
+                </li>
+                <li>
+                    <a href="https://elchininet.github.io/isometric/">
+                        isometric
+                    </a>
+                </li>
+            </ul>
+            <p>More cool stuff</p>
+            <ul>
+                <li>
+                    <a href="https://github.com/raphamorim/awesome-canvas/blob/main/README.md">
+                        Awesome Canvas
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/sindresorhus/awesome-nodejs?tab=readme-ov-file#readme">
+                        Awesome Node
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/sindresorhus/awesome">Awesome</a>
+                </li>
+            </ul>
         </div>
     );
 };
